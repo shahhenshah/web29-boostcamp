89041884250cd79f30afc906131bc7ef67bc311d
caffesale <pglibrary80@gmail.com>
2026-01-08 16:07:51 +0900

    chore: s3, cloudfront 서비스unit 페이지 분리

---
+++ apps/web/src/app/cloudfrontsample/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import CloudFrontStepsContainer from '@/components/cloudfront/cloudfront-steps-container'
+
+export default function SamplePage() {
+  return <CloudFrontStepsContainer />
+}
---
+++ apps/web/src/app/s3sample/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import S3StepsContainer from '@/components/s3/s3-steps-container'
+
+export default function SamplePage() {
+  return <S3StepsContainer />
+}
